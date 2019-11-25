9b86eb350b90e8d7b14803be937e6a092d5fe34d
nahianalam18 <41264226+nahianalam18@users.noreply.github.com>
2019-11-24 21:33:24 -0600

    Add files via upload
    Assignment Completed

--- mfs.c
+++ mfs.c
@@ -8,6 +8,9 @@
 #define BLOCK_SIZE 8192
 #define NUM_BLOCKS 4226
 #define NUM_FILES 128
+#define MAX_COMMAND_SIZE 255
+#define MAX_NUM_ARGUMENTS 5     // Mav shell only supports five arguments
+#define WHITESPACE " \t\n"
 
 FILE *fd;
 
@@ -116,15 +119,24 @@ void delete(char * filename)
   int i;
   for(i = 0; i < NUM_FILES ; i++)
   {
-    if(inodeList[dir[i].inode].attributes == 2 )
-      {printf("del: That file is marked read-only.\n"); break;}
-    else if(strcmp(dir[i].name,filename) == 0)
+    // if(inodeList[dir[i].inode].attributes == 2  || inodeList[dir[i].inode].attributes == 3 )
+    //   {printf("del: That file is marked read-only.\n"); break;}
+     if(strcmp(dir[i].name,filename) == 0)
     {
-      dir[i].valid = 0;
-      freeInodeList[dir[i].inode] = 1;
+      
       break;
     }
   }
+  if(inodeList[dir[i].inode].attributes == 2  || inodeList[dir[i].inode].attributes == 3 )
+      {printf("del: That file is marked read-only.\n"); return;}
+
+      else
+      {
+        dir[i].valid = 0;
+      freeInodeList[dir[i].inode] = 1;
+      }
+      
+
 }
 
 void list()
@@ -133,9 +145,11 @@ void list()
 
   for (i = 0; i < NUM_FILES; i++)
   {
-    if (dir[i].valid == 1 && inodeList[dir[i].inode].attributes != 1)
+    if (dir[i].valid == 1 && inodeList[dir[i].inode].attributes != 1 && inodeList[dir[i].inode].attributes != 3 )
     {
+     // if(inodeList[dir[i].inode].attributes != 1)
       printf("%d %s %s", inodeList[dir[i].inode].size, dir[i].name, inodeList[dir[i].inode].time);
+
     }
   }
 }
@@ -172,9 +186,24 @@ void createfs(char *filename)
 
 void open(char *filename)
 {
+  int status;      // Hold the status of all return values.
+  struct stat buf; // stat struct to hold the returns from the stat call
+
+  status = stat(filename, &buf);
+
+  if (status != -1)
+  {
+
   fd = fopen(filename, "r");
   fread(blocks, BLOCK_SIZE, NUM_BLOCKS, fd);
   fclose(fd);
+  }
+  else
+  {
+    printf("open error: File does not exist\n");
+    return;
+  }
+  
 }
 
 void attrib(char *aType, char *filename)
@@ -189,13 +218,56 @@ void attrib(char *aType, char *filename)
     }
   }
     if (strcmp(aType, "+h") == 0)
-      inodeList[dir[i].inode].attributes = 1;
+    {
+      if(inodeList[dir[i].inode].attributes == 2 || inodeList[dir[i].inode].attributes == 3)
+      {
+        inodeList[dir[i].inode].attributes = 3; 
+      }
+
+      else 
+      {
+        inodeList[dir[i].inode].attributes = 1;
+      }
+
+    }
+      
     else if (strcmp(aType, "-h") == 0)
-      inodeList[dir[i].inode].attributes = 0; 
+    {
+      if(inodeList[dir[i].inode].attributes == 3 || inodeList[dir[i].inode].attributes == 2)
+      {
+        inodeList[dir[i].inode].attributes = 2;
+      }
+
+      else 
+      {
+        inodeList[dir[i].inode].attributes = 0;
+      }
+    }
+      
     else if (strcmp(aType, "+r") == 0)
-      inodeList[dir[i].inode].attributes = 2; 
+    {
+      if(inodeList[dir[i].inode].attributes == 1 || inodeList[dir[i].inode].attributes == 3)
+      {
+        inodeList[dir[i].inode].attributes = 3; 
+      }
+      else 
+      {
+        inodeList[dir[i].inode].attributes = 2;
+      }
+    }
+      
     else if (strcmp(aType, "-r") == 0)
-      inodeList[dir[i].inode].attributes = 0; 
+      {
+      if(inodeList[dir[i].inode].attributes == 3 || inodeList[dir[i].inode].attributes == 1 )
+      {
+        inodeList[dir[i].inode].attributes = 1;
+      }
+
+      else 
+      {
+        inodeList[dir[i].inode].attributes = 0;
+      }
+    }
 
      
     
@@ -217,14 +289,26 @@ long df()
 
 void close(char *filename)
 {
+  int status;      // Hold the status of all return values.
+  struct stat buf; // stat struct to hold the returns from the stat call
+
+  status = stat(filename, &buf);
+
+  if (status != -1)
+  {
   fd = fopen(filename, "w");
   fwrite(blocks, BLOCK_SIZE, NUM_BLOCKS, fd);
   fclose(fd);
   memset(blocks, 0, NUM_BLOCKS * BLOCK_SIZE);
+  }
+  else 
+  {
+    printf("close error: File does not exist.\n");
+    return;
+  }
 }
 
 int get(char * source, char*destination)
-
 {
 
   int    status;                   // Hold the status of all return values.
@@ -234,7 +318,7 @@ int get(char * source, char*destination)
   // allow us to get the file size. We also get interesting file system info about the
   // file such as inode number, block size, and number of blocks.  For now, we don't 
   // care about anything but the filesize.
-  status =  stat( source, &buf ); 
+  //status =  stat( source, &buf ); 
 
   int i;
 
@@ -242,6 +326,12 @@ int get(char * source, char*destination)
   {
     if(strcmp(dir[i].name,source)== 0)
       break;
+  }
+
+  if(strcmp(dir[i].name,"") == 0)
+  {
+    printf("get error: %s not in the File System!\n",source);
+    return -1;
   }
 
   printf("The file I am about to get from the file system is %s\n",dir[i].name);
@@ -265,10 +355,10 @@ int get(char * source, char*destination)
 
     // Initialize our offsets and pointers just we did above when reading from the file.
     int block_index = 0;
-    int copy_size   = buf . st_size;
+    int copy_size   = inodeList[dir[i].inode].size;
     int offset      = 0;
 
-   printf("Writing %d bytes to %s\n", (int) buf . st_size, destination );
+   printf("Writing %d bytes to %s\n", copy_size, destination );
 
   //   // Using copy_size as a count to determine when we've copied enough bytes to the output file.
   //   // Each time through the loop, except the last time, we will copy BLOCK_SIZE number of bytes from
@@ -310,13 +400,102 @@ int get(char * source, char*destination)
   //   // Close the output file, we're done. 
      fclose( ofp );
   }
-  // else
+
+
+  int Get(char * source)
+{
+
+  int    status;                   // Hold the status of all return values.
+  struct stat buf;                 // stat struct to hold the returns from the stat call
+
+  // Call stat with out input filename to verify that the file exists.  It will also 
+  // allow us to get the file size. We also get interesting file system info about the
+  // file such as inode number, block size, and number of blocks.  For now, we don't 
+  // care about anything but the filesize.
+  //status =  stat( source, &buf ); 
+
+  int i;
+
+  for(i = 0; i < NUM_FILES ; i++)
+  {
+    if(strcmp(dir[i].name,source)== 0)
+      break;
+  }
+
+  if(strcmp(dir[i].name,"") == 0)
+  {
+    printf("get error: %s not in the File System!\n",source);
+    return -1;
+  }
+
+  printf("The file I am about to get from the file system is %s\n",dir[i].name);
+
+
+
+
+
+  // If stat did not return -1 then we know the input file exists and we can use it.
+  // if( status != -1 )
   // {
-  //   printf("Unable to open file: %s\n", source );
-  //   perror("Opening the input file returned: ");
-  //   return -1;
-  // }
-//}
+  FILE *ofp;
+    ofp = fopen(source, "w");
+
+    if( ofp == NULL )
+    {
+      printf("Could not open output file: %s\n", source );
+      perror("Opening output file returned");
+      return -1;
+    }
+
+    // Initialize our offsets and pointers just we did above when reading from the file.
+    int block_index = 0;
+    int copy_size   = inodeList[dir[i].inode].size;
+    int offset      = 0;
+
+   printf("Writing %d bytes to %s\n", copy_size, source );
+
+  //   // Using copy_size as a count to determine when we've copied enough bytes to the output file.
+  //   // Each time through the loop, except the last time, we will copy BLOCK_SIZE number of bytes from
+  //   // our stored data to the file fp, then we will increment the offset into the file we are writing to.
+  //   // On the last iteration of the loop, instead of copying BLOCK_SIZE number of bytes we just copy
+  //   // how ever much is remaining ( copy_size % BLOCK_SIZE ).  If we just copied BLOCK_SIZE on the
+  //   // last iteration we'd end up with gibberish at the end of our file. 
+    while( copy_size > 0 )
+    { 
+
+      int num_bytes;
+
+      // If the remaining number of bytes we need to copy is less than BLOCK_SIZE then
+      // only copy the amount that remains. If we copied BLOCK_SIZE number of bytes we'd
+      // end up with garbage at the end of the file.
+      if( copy_size < BLOCK_SIZE )
+      {
+        num_bytes = copy_size;
+      }
+      else 
+      {
+        num_bytes = BLOCK_SIZE;
+      }
+
+      // Write num_bytes number of bytes from our data array into our output file.
+      fwrite( &blocks[inodeList[dir[i].inode].blocks[block_index]], num_bytes, 1, ofp ); 
+
+      // Reduce the amount of bytes remaining to copy, increase the offset into the file
+      // and increment the block_index to move us to the next data block.
+      copy_size -= BLOCK_SIZE;
+      offset    += BLOCK_SIZE;
+      block_index ++;
+
+      // Since we've copied from the point pointed to by our current file pointer, increment
+      // offset number of bytes so we will be ready to copy to the next area of our output file.
+      fseek( ofp, offset, SEEK_SET );
+    }
+
+  //   // Close the output file, we're done. 
+     fclose( ofp );
+  }
+ 
+
 
 
 int put(char *source)
@@ -333,23 +512,24 @@ int put(char *source)
   {
 
     int i;
-
-    // for(i = 0 ; i < NUM_FILES ; i++)
-    // {
-    //   if(strcmp(dir[i].name,source) == 0)
-    //   printf("put error: File already in the system!\n");
-    //   return -1;
-    // }
     int currentFileSize = (int)buf.st_size;
+    size_t length;
+    length = strlen(source);
 
     if(currentFileSize >= 10,240,000)
     {
-      printf("put error: File size too big.\n");
+      printf("put error: Not enough disk space.\n");
       return -1;
     }
     if(currentFileSize > df())
     {
       printf("put error: Not enough disk space.\n");
+      return -1;
+    }
+
+    if(length>32)
+    {
+      printf("put error: File name too long\n");
       return -1;
     }
     int freeBlockIndex = findFreeDirectory();
@@ -364,7 +544,7 @@ int put(char *source)
     strcpy(inodeList[freeInodeIndex].time, ctime(&buf.st_atime));
 
     FILE *ifp = fopen(source, "r");
-    printf("Reading %d bytes from %s\n", currentFileSize, source);
+    // printf("Reading %d bytes from %s\n", currentFileSize, source);
 
     int offset = 0;
 
@@ -433,19 +613,158 @@ int main()
 
   int i;
 
-  // createfs("disk.image");
-  open("disk.image");
- // put("lama.txt");
- // delete("testFile.txt");
- // attrib("-h","jackal.txt");
-  // delete("lama.txt");
-//  list();
+ // inodeList = (struct Inode*)&blocks[i+3][0]; 
 
-//  get("oregano.txt","gay.txt");
+  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
+ // void* fileBlock = malloc(BLOCK_SIZE * NUM_BLOCKS);
+  while( 1 )
+  {
+    // Print out the mfs prompt
+    printf ("msh> ");
 
-// printf("Size = %ld\n",df());
+    // Read the command from the commandline.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something since fgets returns NULL when there
+    // is no input
+    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
+
+    /* Parse input */
+    char *token[MAX_NUM_ARGUMENTS];
+
+    int   token_count = 0;                                 
+                                                           
+    // Pointer to point to the token
+    // parsed by strsep
+    char *arg_ptr;                                         
+                                                           
+    char *working_str  = strdup( cmd_str );                
+
+    // we are going to move the working_str pointer so
+    // keep track of its original value so we can deallocate
+    // the correct amount at the end
+    char *working_root = working_str;
+
+    // Tokenize the input stringswith whitespace used as the delimiter
+    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
+              (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
+        token_count++;
+    }
+
+    // Now print the tokenized input as a debug check
+    // \TODO Remove this code and replace with your shell functionality
+
+    int token_index  = 0;
+    for( token_index = 0; token_index < token_count; token_index ++ ) 
+    {
+      //printf("\n%s %d\n",token[token_index],token_index);
+      if(token[token_index]!= NULL && (strcmp(token[token_index],"exit") == 0 || strcmp(token[token_index],"quit") == 0))
+      {
+        exit(0);
+      }
+     else if(token[token_index]!= NULL && strcmp(token[token_index],"createfs") == 0)
+      {
+        if(token[token_index+1] == NULL)
+          {printf("createfs error: Please enter name of File System\n");break;}
+        createfs(token[token_index+1]);
+      }
+
+     else if(token[token_index]!= NULL && strcmp(token[token_index],"put") == 0)
+      {
+        if(token[token_index+1] == NULL)
+          {printf("put error: Please enter name of file\n");break;}
+        put(token[token_index+1]);
+      }
+     else if(token[token_index]!= NULL && strcmp(token[token_index],"list") == 0)
+      {
+        list();
+      }
+
+       else if(token[token_index]!= NULL && strcmp(token[token_index],"open") == 0)
+      {
+        if(token[token_index+1] == NULL)
+          {printf("open error: Please enter name of file to be opened\n");break;}
+        open(token[token_index+1]);
+      }
+
+      else if(token[token_index]!= NULL && strcmp(token[token_index],"close") == 0)
+      {
+        if(token[token_index+1] == NULL)
+          {printf("close error: Please enter name of file to be closed\n");break;}
+        close(token[token_index+1]);
+      }
+
+      else if(token[token_index]!= NULL && strcmp(token[token_index],"delete") == 0)
+      {
+        if(token[token_index+1] == NULL)
+          {printf("delete error: Please enter name of file to be deleted\n");break;}
+        delete(token[token_index+1]);
+      }
+      else if(token[token_index]!= NULL && strcmp(token[token_index],"df") == 0)
+      {
+        printf("%ld\n",df());
+      }
+      else if(token[token_index]!= NULL && strcmp(token[token_index],"attrib") == 0)
+      {
+        if(token[token_index+1] == NULL)
+          {printf("attrib error: Type of attrib missing\n");break;}
+        else if(token[token_index+2] == NULL)
+          {printf("attrib error: Filename missing\n");break;}
+        attrib(token[token_index+1],token[token_index+2]);
+      }
+
+       else if(token[token_index]!= NULL && strcmp(token[token_index],"get") == 0)
+      {
+        if(token[token_index+1] == NULL)
+          {printf("get error: filename missing\n");break;}
+       if(token[token_index+2] == NULL)
+          {
+            Get(token[token_index+1]);
+          }
+          else
+          {
+            get(token[token_index+1],token[token_index+2]);
+          }
+      }
+
+      
+     // printf("token[%d] = %s\n", token_index, token[token_index] );  
+    }
+
+    free( working_root );
+
+  }
+  return 0;
+
+//   // createfs("disk.image");
+//   open("disk.image");
+//  // put("jackal.txt");
+//  //delete("samisGay.txt");
+// // attrib("-r","samisGay.txt");
+//   // delete("lama.txt");
+//   list();
+  
+//   printf("\n\n");
+//  for(i = 0 ; i < NUM_FILES ; i++)
+//  {
+//    if(dir[i].valid == 1)
+//    printf("%d : %s %d\n",i,dir[i].name,inodeList[dir[i].inode].attributes);
+//  }
+
+//  //get("job.txt","samisGay.txt");
+//  //get("lama.txt","LULU.txt");
+
+// // Get("job.txt");
+
+// // printf("Size = %ld\n",df());
   
   
 
-  close("disk.image");
+//   close("disk.image");
 }
